d2b0b941640f39ad014ceaf77e114a7589c905e7
IRFAN-KHAN-git <irfanprof9@gmail.com>
2025-02-21 21:32:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getDatabase, ref , push , onValue , remove } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js";
+
+const firebaseConfig = {
+
+    databaseURL: "https://lead-tracker-app-33c1d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const refFromDB = ref( database , "leads")
+
+
+const inputel = document.getElementById("input-el")
+const inputbtn = document.getElementById("input-btn")
+const listel = document.getElementById("listel")
+const delbtn = document.getElementById("del-btn")
+
+    
+onValue(refFromDB , function(snapshot) {
+    if(snapshot.exists()){
+    const snapVal = snapshot.val()
+    const leads = Object.values(snapVal)
+    showLeads(leads)
+    }
+})
+
+inputbtn.addEventListener("click",function(){
+              push(refFromDB , inputel.value)
+        inputel.value=""
+        
+        
+})
+
+function showLeads(leads){
+    let list=""
+    for(let i=0;i<leads.length;i++){
+        list += `
+            <li>
+               
+               <button id="del-btn"><a target='_blank' href='${leads[i]}'>
+               ${leads[i]}
+               </a></button>
+           </li>    
+       `
+    }
+    listel.innerHTML=list    
+}
+
+delbtn.addEventListener("dblclick",function(){
+    
+
+remove(refFromDB)
+listel.innerHTML=""
+
+})
